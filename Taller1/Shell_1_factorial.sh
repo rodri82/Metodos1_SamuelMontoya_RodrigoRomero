711976dacdac6183a46ed159245692e3eef84e1a
rodri82 <r.romerop@uniandes.edu.co>
2022-08-20 08:26:10 -0500

    Taller 1 - Shell - Factorial

---
+++ Taller1/Shell_1_factorial.sh
@@ -0,0 +1,16 @@
+n=$1
+cont=1
+factorial=1
+if [ $# -ne 1 ]||[ $n -lt 0 ]; then
+	echo Se requiere un '(1)' número positivo
+	exit 1
+fi
+
+if [ $n -gt 1 ]; then
+	while [ $cont -le $n ]
+	do
+		let factorial=$cont*$factorial
+		let cont=$cont+1
+	done
+fi
+echo El factorial de $n '('$n'!)' es $factorial
